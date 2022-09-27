f9e2aab2c0254d03eb667de0eaba0ef1cbe31fa5
Kyaw-Min-Thant <kyawminthant5191@gmail.com>
2022-09-27 21:26:17 +0630

    update

--- app.js
+++ app.js
@@ -10,35 +10,24 @@ const localhost = '127.0.0.1'
 
 
 app.get("",(req,res)=>{
-    res.send("<h1>Hello Sayar nice to meet you</h1>");
+    res.send("Hello this is for assignment");
 })
 
 app.get("/about",(req,res)=>{
-    res.send({
-        Name:"Kyaw Min Thant",
-        Hobby:"Reading Book",
-        age: 17
-    })
-
-})
-
-app.get("/KyawMinThant",(req,res)=>{
-    res.send("<h1>This is from KyawMinThant Page</h1>");
+    res.sendFile(__dirname + '/about.html')
 })
 
 app.get("/lenovo",(req,res)=>{
-    res.send("<h1>This is write with lenovo ideapad 3</h1>");
+    res.sendFile(__dirname + '/lenovo.html');
 })
 
-app.get("/bytheway",(req,res)=>{
-    res.send("<h1>By the way I like writing code too.</h1>");
-})
-
-app.get("/index.html", (req, res) => {
+app.get("/index", (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-
+app.get("/song", (req, res) => {
+    res.sendFile(__dirname + '/song.html')
+})
 
 app.listen(port, localhost,()=>{
     console.log(`Server running at http://${localhost}:${port}`);
